6c3513cb309d2f4a2adc325e2d8ba11fce047d6d
turgutAgha <turgut.aghashov.std@bhos.edu.az>
2022-09-28 11:11:47 -0700

    commit

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,23 +3,29 @@ package org.example;
 import static org.example.PermissionLevel.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 
 public class MainTest {
-    PermissionManager manager = new PermissionManager();
-    @Test
-    void testGet() {
-        assertEquals("USER", manager.get());
+    PermissionManager manager;
+    HashMap<PermissionLevel, String> permissions = new HashMap<>();
+
+    @BeforeEach
+    void setup() {
+        manager = new PermissionManager();
+        permissions.put(PermissionLevel.ADMIN, "ADMIN");
+        permissions.put(PermissionLevel.USER, "USER");
+        permissions.put(PermissionLevel.DEVELOPER, "DEVELOPER");
     }
 
     @Test
     void testSet() {
-        manager.set(ADMIN);
-        assertEquals("ADMIN", manager.get());
-        manager.set(USER);
-        assertEquals("USER", manager.get());
-        manager.set(DEVELOPER);
-        assertEquals("DEVELOPER", manager.get());
+        for (PermissionLevel permissionLevel: permissions.keySet()) {
+            manager.set(permissionLevel);
+            assertEquals(permissions.get(permissionLevel), manager.get());
+        }
     }
 }
